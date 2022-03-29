9517e80b3fe860d25b0a5b2e7e9ddefe90712db5
Hannah <hannahmgrout@gmail.com>
2022-03-28 19:16:12 -0700

    commit tree 2c63c29620cfcb5e2047cb11b624316a977503f7

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    let results = drivers.filter(name => name.toLowerCase() === string.toLowerCase())
+    return results
+}
+
+function fuzzyMatch(drivers, string) {
+    let results = drivers.filter(name => name.substring(0,2) === string.substring(0,2))
+    return results
+}
+
+function matchName(drivers, string) {
+    let results = drivers.filter(drivers => drivers.name === string)
+    return results
+}
